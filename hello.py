ceebf379c500fd81b73b5849f0ac3e854100eba7
mr-shelby321 <xylacap@gmail.com>
2024-05-13 23:38:18 +0000

    modify: hello.py

--- hello.py
+++ hello.py
@@ -1,9 +1,12 @@
 def addNumbers(num1,num2):
     return num1 + num2
 
+def substractNumbers(num1,num2):
+    return num1 - num2
+
 number1 = int(input(">"))
 
 number2 = int(input(">"))
 
-print(addNumbers(number1, number2))
+print(substractNumbers(number1, number2))
 
