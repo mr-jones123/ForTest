c149e18cf75f63ab96138a2b88b7792276816f67
mr-jones123 <lacapxyniljhed@gmail.com>
2024-05-14 08:06:01 +0800

    add: word.py modify: hello.py

--- hello.py
+++ hello.py
@@ -4,9 +4,16 @@ def addNumbers(num1,num2):
 def substractNumbers(num1,num2):
     return num1 - num2
 
+def divideNumbers(num1, num2):
+    if num2 == 0:
+        return "Error: Division by zero is not allowed"
+    return num1 / num2
 number1 = int(input(">"))
 
 number2 = int(input(">"))
 
 print(substractNumbers(number1, number2))
+
+print(addNumbers(number1, number2))
+
 
